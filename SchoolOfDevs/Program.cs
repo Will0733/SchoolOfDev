917b9756faa080be6d5aab4359c9a7c5f6ba157a
Will0733 <wilker.silvaaus@gmail.com>
2022-02-13 21:12:20 +1100

    Service  and Controller User

--- SchoolOfDevs/Program.cs
+++ SchoolOfDevs/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SchoolOfDevs.Helpers;
+using SchoolOfDevs.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserService, UserService>();
+
+
 
 var app = builder.Build();
 
---
+++ SchoolOfDevs/Services/UserService.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolOfDevs.Helpers;
+using SchoolOfDevs.Model;
+
+namespace SchoolOfDevs.Services
+{
+    public interface IUserService
+    {
+        public Task<User> Create(User user);
+        public Task<User> GetById(int id);
+        public Task Update(User userIn, int id);
+        public Task Delete(int id);
+        public Task<List<User>> GetAll();
+    }
+    public class UserService : IUserService
+    {
+        private readonly DataContext _context;
+        public UserService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Create(User user)
+        {
+            User userDb = await _context.Users
+                .AsNoTracking()
+                .SingleOrDefaultAsync(u => u.UserName == user.UserName);
+            if (userDb is not null)
+                throw new Exception($"User name {user.UserName} already exist.");
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task Delete(int id)
+        {
+            User userDb = await _context.Users
+                .SingleOrDefaultAsync(u => u.Id == id);
+            if (userDb is null)
+                throw new Exception($"User {id} not found.");
+            _context.Users.Remove(userDb);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> GetAll() => await _context.Users.ToListAsync();
+
+
+        public async Task<User> GetById(int id)
+        {
+            User userDb = await _context.Users
+                .SingleOrDefaultAsync(u => u.Id == id);
+
+            if (userDb is null)
+                throw new Exception($"User {id} not found.");
+
+            return userDb;
+        }
+
+        public async Task Update(User userIn, int id)
+        {
+            if (userIn.Id != id)
+                throw new Exception("Router id differs User id.");
+
+            User userDb = await _context.Users
+                .AsNoTracking()
+                .SingleOrDefaultAsync(u => u.Id == id);
+
+            if (userDb is null)
+                throw new Exception($"User {id} not found.");
+
+            _context.Entry(userIn).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
